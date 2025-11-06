2f30de51c74744a191f22965d305b1b5cc673a5c
devpandai <firman@pandai.id>
2025-11-06 20:13:35 +0700

    update

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "malam kaka pimen, gimana kabar ?")
+	fmt.Fprintf(w, "halaman ini hanya testing")
 }
 
 func main() {
